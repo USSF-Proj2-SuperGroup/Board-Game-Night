afa1ba7cd740be5b8401378bd769f3102e88a20b
Peter Vayda <petertvayda@gmail.com>
2020-12-15 11:42:39 -0800

    added groupvote boilerplate project

---
+++ groupvote/src/test/java/com/supergroup/groupvote/GroupvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supergroup.groupvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
