75882a8935c8130a4cbce66947f6529bb65c0fd6
Donna <dlfarris97@gmail.com>
2020-12-15 16:51:00 -0500

    added springboot files and folders

---
+++ src/main/java/com/example/boardgamenightinventoryservice/BoardGameNightInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardgamenightinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardGameNightInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardGameNightInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boardgamenightinventoryservice/BoardGameNightInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardgamenightinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameNightInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
