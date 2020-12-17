fc4ac43c2227ff5cc3df393eb38bc6ad4a098779
Robert-F-Cameron <robert.f.cameron@gmail.com>
2020-12-16 20:30:25 -0800

    frontend patches

---
+++ registration-react/src/services/auth-header.js
@@ -0,0 +1,14 @@
+import { useReducer } from "react";
+
+export default function authHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+  
+    if (user && user.accessToken) {
+      return { 
+        Authorization: 'Bearer ' + user.accessToken,
+        ID: user.id
+        };
+    } else {
+      return {};
+    }
+  }
