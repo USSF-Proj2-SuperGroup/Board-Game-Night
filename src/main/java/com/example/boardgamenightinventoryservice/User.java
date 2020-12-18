d0e30b656c0f28c7a19b35f4ffbc18de4d4bfc2b
donnafarris <dlfarris97@gmail.com>
2020-12-18 12:54:24 -0500

    updated User and UserGame

--- src/main/java/com/example/boardgamenightinventoryservice/User.java
+++ src/main/java/com/example/boardgamenightinventoryservice/User.java
@@ -12,7 +12,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String username;
 
@@ -29,10 +29,10 @@ public class User {
         this.password = password;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
--- src/main/java/com/example/boardgamenightinventoryservice/UserGame.java
+++ src/main/java/com/example/boardgamenightinventoryservice/UserGame.java
@@ -1,4 +1,44 @@
 package com.example.boardgamenightinventoryservice;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_games")
 public class UserGame {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User userGames;
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    Game gamesU;
+
+    public String getGameApiId(){
+        return gamesU.getGame_api_id();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUserGames() {
+        return userGames;
+    }
+    public void setUserGames(User userGames) {
+        this.userGames = userGames;
+    }
+
+    public Game getGamesU() {
+        return gamesU;
+    }
+    public void setGamesU(Game gamesU) {
+        this.gamesU = gamesU;
+    }
 }
